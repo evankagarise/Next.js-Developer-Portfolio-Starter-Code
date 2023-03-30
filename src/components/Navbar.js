7a7e78acedfbd018b92966c2888c55cdb04955b3
evan <95433637+evankagarise@users.noreply.github.com>
2023-03-30 15:26:52 -0400

    icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,41 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import {useRouter} from 'next/router'
+import { DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, TwitterIcon } from './Icons'
 
-const CustomLink = ()
+const CustomLink = ({href, title, className=""}) => {
+    const router = useRouter()
+
+    return (
+        <Link href={href} className={`${className} relative group`}>
+            {title}
+
+            <span  className={`h-[1px] inline-block  bg-dark absolute left-0 
+            -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
+        </Link>
+    )
+}
 const Navbar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between '>
         <nav>
-            <Link href="/home" className='mx-4'>Home
-            <span className='h-[1px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300'>&nbsp;</span>
-            </Link>
-            <Link href="/about" className='mx-4'>About</Link>
-            <Link href="/projects" className='mx-4'>Projects</Link>
-            <Link href="/articles" className='mx-4'>Blog</Link>
+           <CustomLink href='/' title="Home" className='mr-4' />
+           <CustomLink href='/about' title="About" className='mx-4' />
+           <CustomLink href='/projects' title="Projects" className='mx-4' />
+           <CustomLink href='/articles' title="Articles" className='ml-4' />
         </nav>
         <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
              <Logo />
         </div>
       
-        <nav>
-            <Link href="/" target={"_blank"} >T</Link>
-            <Link href="/" target={"_blank"} >T</Link>
-            <Link href="/" target={"_blank"} >T</Link>
-            <Link href="/" target={"_blank"} >T</Link>
-            <Link href="/" target={"_blank"} >T</Link>
-            <Link href="/" target={"_blank"} >T</Link>
+        <nav className='flex items-center justify-center flex-wrap gap-2'>
+            <Link href="/" target={"_blank"} ><TwitterIcon /></Link>
+            <Link href="/" target={"_blank"} ><GithubIcon /></Link>
+            <Link href="/" target={"_blank"} ><LinkedInIcon /></Link>
+            <Link href="/" target={"_blank"} ><PinterestIcon /></Link>
+            <Link href="/" target={"_blank"} ><DribbbleIcon /></Link>
+           
         </nav>
     </header>
   )
